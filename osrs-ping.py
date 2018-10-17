6e1b29f0f9e2be1cbd5aa3a9147529a10333366d
Edward Parkinson <e.j.parkinson@soton.ac.uk>
2018-10-17 19:37:02 +0100

    Updating with working Unix version - but probably broke Windows support :^).

--- osrs-ping.py
+++ osrs-ping.py
@@ -4,7 +4,7 @@ from platform import system
 from subprocess import Popen, PIPE
 
 
-def osrs_avg_ping(world_number, n_tests=5):
+def world_avg_ping(world_number, n_tests=5):
     """
     Returns the average latency of an OldSchool RuneScape server.
 
@@ -22,34 +22,49 @@ def osrs_avg_ping(world_number, n_tests=5):
         measured in milliseconds.
     """
 
-    assert(type(world_number) == int)
-    assert(type(n_tests) == int)
+    assert(type(world_number) == int), "World type not an integer."
+    assert(type(n_tests) == int), "Number of tests not an integer."
+    assert(n_tests > 1), "The number of tests has to be greater than one."
 
-    # create a ping command
-    host = 'oldschool' + str(world_number) + '.runescape.com'
-    ping_param = '-n {} '.format(n_tests) if system().lower() == 'windows' \
-        else '-c {} '.format(n_tests)
-    ping_command = 'ping ' + ping_param + host
+    # Create the ping command to send to the terminal - note that the repeat command is different on Windows and Unix
+    # based operating systems
+    hostname = "oldschool{}.runescape.com".format(world_number)
+    if system().lower == "windows":
+      repeat = "-n {}".format(n_tests)
+    else:
+      repeat = "-c {}".format(n_tests)
+    ping_command = "ping {} {}".format(repeat, hostname)
 
-    ping = Popen(ping_command, stdout=PIPE, shell=True)
-    output = ping.communicate()[0].split()
+    print("Pinging: {} {}".format(repeat, hostname))
 
+    # Now send the ping command to the terminal and capture stdout and stderr, then decode the bytes string into UTF-8
+    # and split the list
+    ping = Popen(ping_command, stdout=PIPE, stderr=PIPE, shell=True)
+    stdout, stderr = ping.communicate()
+    stdout_split = stdout.decode("utf-8").split()
+
+    # Now we need to find where the average latency is output - note that this is different between Windows and Unix
+    # based operating systems again
     if system().lower == 'windows':
-        if output[-1] == b'loss),':
+        if stdout[-1] == b'loss),':
             avg_latency = 999
         else:
-            avg_latency = int(''.join(findall(r'\d', str(output[-1]))))
+            avg_latency = int(''.join(findall(r'\d', str(stdout[-1]))))
     else:
-        print(output[-2])
-        avg_latency = 999
+        avg_ping_idx = -1
+        for i in range(len(stdout_split)):
+            if stdout_split[i] == "min/avg/max/stddev":  # When we find this, the time is 2 entries next
+                avg_ping_idx = i + 2
+                break
+        if avg_ping_idx == -1:
+            raise IndexError("Couldn't find average latency for {}".format(hostname))
+        avg_latency = stdout_split[avg_ping_idx].replace("/", " ").split()[1]
 
     return avg_latency
 
 
-if __name__ == '__main__':
-
+def main():
     n_args = len(argv)
-
     if argv[1] == 'uk':
         print('Pinging all non-PvP UK worlds.')
         worlds = [2, 9, 10, 17, 18, 33, 34, 41, 42, 49, 50, 58, 65, 66, 73]
@@ -66,20 +81,25 @@ if __name__ == '__main__':
         raise ValueError()
 
     if n_args == 2:
-        print('Number of ping tests not provided. Using default value: 1.')
-        n_tests = 1
+        n_tests = 5
+        print('Number of ping tests not provided. Using default value: {}.'.format(n_tests))
     else:
         n_tests = int(argv[2])
 
+    if (n_tests > 5):
+        print ("Pinging each world {} times, this may take a while.".format(n_tests))
     pings = []
     for world in worlds:
-        world_ping = osrs_avg_ping(world, n_tests)
+        world_ping = world_avg_ping(world, n_tests)
         pings.append("World {}".format(world))
         pings.append(world_ping)
 
-    # the latency is index 1, 3, 5, ... etc
+    # The latency is index 1, 3, 5, ... etc and 0, 2, 4, .. etc is the world number
     lowest_ping = min(pings[1::2])
-    lowest_ping_world = pings[pings.index(lowest_ping) - 1]
+    lowest_ping_world_number = pings[pings.index(lowest_ping) - 1]
+    print('\nLowest ping:\n{} {} ms'.format(lowest_ping_world_number, lowest_ping))
 
-    print('\nLowest ping:\n{}, ping = {} ms'.format(
-            lowest_ping_world, lowest_ping))
+    return
+
+if __name__ == '__main__':
+    main()
